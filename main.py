f349dd3026685de1c4bc7bdb9b385e8f2535934c
amawlid <ahmedmawlidahmed@gmail.com>
2025-04-15 14:02:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from tkinter import *
 from tkinter import ttk 
+from datetime import datetime 
 
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm, text="Testing Tkinter!").grid(column=0,row=0)
-ttk.Button(frm, text="Stop", command=root.destroy).grid(column=1,row=0)
+ttk.Button(frm, text="Stop", command=root.destroy).grid(column=0,row=0)
+x = datetime.today().strftime("%Y-%m-%d") 
+ttk.Label(frm, text= x).grid(column=0,row=1)
+
 root.mainloop()
